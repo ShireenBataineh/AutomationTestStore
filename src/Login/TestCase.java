ac9999cabf07b527e94c2c9fb27e55240b024e24
ShireenBataineh <batainehshereen1@gmail.com>
2025-10-19 13:09:38 +0300

    update

--- src/Login/TestCase.java
+++ src/Login/TestCase.java
@@ -1,11 +1,14 @@
 package Login;
 
+import java.util.Random;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.Select;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
@@ -13,7 +16,7 @@ import org.testng.annotations.Test;
 public class TestCase {
 	
 	WebDriver driver= new ChromeDriver();
-	
+	Random rand=new Random();
 	
 	@BeforeTest
 	public void beforeTest() throws InterruptedException
@@ -24,43 +27,94 @@ public class TestCase {
 	}
 	
 	@Test(priority=1)
-	public void signUp()
+	public void signUp() throws InterruptedException
 	{
 //		WebElement signUp=driver.findElement(By.id("customer_menu_top"));
 //		signUp.click();
 //		WebElement continue1=driver.findElement(By.className("btn btn-orange pull-right"));
 //		continue1.click();
 		driver.navigate().to("https://automationteststore.com/index.php?rt=account/create");
+		//FirstName
+		String [] FNames= {"Shireen","Khawla","Reta","Mera","Roaa","Mary","Sewar"};
+		int num1=rand.nextInt(FNames.length);
 		WebElement firstName=driver.findElement(By.id("AccountFrm_firstname"));
-		firstName.sendKeys("Shireen");
+		firstName.sendKeys(FNames[num1]);
+		//LastName
+		String [] LNames= {"Abdullah","Mohammad","Zaid","Ahmad","Anas"};
+		int num2=rand.nextInt(LNames.length);
 		WebElement lastName=driver.findElement(By.id("AccountFrm_lastname"));
-		lastName.sendKeys("Bataineh");
-		WebElement email=driver.findElement(By.id("AccountFrm_email"));
-		email.sendKeys("she_bata@yahoo.com");
+		lastName.sendKeys(LNames[num2]);
+		//Email
+		String email=FNames[num1]+LNames[num2]+rand.nextInt(500)+"@gmail.com";
+		WebElement emailInput=driver.findElement(By.id("AccountFrm_email"));
+		emailInput.sendKeys(email);
+		//Telephone
+		int tele=rand.nextInt(1000000000);
+		String telle=""+tele;
 		WebElement telephone=driver.findElement(By.id("AccountFrm_telephone"));
-		telephone.sendKeys("0791445979");
-		WebElement fax=driver.findElement(By.id("AccountFrm_fax"));
-		fax.sendKeys("jhfkduri94085");
-		WebElement address1=driver.findElement(By.id("AccountFrm_address_1"));
-		address1.sendKeys("Shmeisani");
-		WebElement city=driver.findElement(By.id("AccountFrm_city"));
-		city.sendKeys("Shmeisani");
+		telephone.sendKeys(telle);
+		//Fax
+		int fax=rand.nextInt(100000);
+		String faxS=""+fax;
+		WebElement faxInput=driver.findElement(By.id("AccountFrm_fax"));
+		faxInput.sendKeys(faxS);
+		//Address1
+		String [] address= {"Shmeisani44","Abdoun55","Sweileh66","AboNuseir77","Khalda88","Gardens99"};
+		int address1=rand.nextInt(address.length);
+		WebElement addressInput=driver.findElement(By.id("AccountFrm_address_1"));
+		addressInput.sendKeys(address[address1]);
+		//City
+		String [] City= {"Aamman","Irbid","Aqaba","Tafileh","Madaba","Salt","Mafraq","Ajloun","Jerash"};
+		int num4=rand.nextInt(City.length);
+		WebElement cityInput=driver.findElement(By.id("AccountFrm_city"));
+		cityInput.sendKeys(City[num4]);
+		
+		
+		
+		//Country
+		WebElement country=driver.findElement(By.id("AccountFrm_country_id"));
+		Select countryS=new Select(country);
+		int countrySize=country.findElements(By.tagName("option")).size();
+		int randomValue1=rand.nextInt(1,countrySize);
+		countryS.selectByIndex(randomValue1);
+		
+		Thread.sleep(1000);
+		
+		//Region/ State
 		WebElement region=driver.findElement(By.id("AccountFrm_zone_id"));
-		region.click();
-		region.sendKeys("3514");
+		Select regionS=new Select(region);
+		int regionSize=region.findElements(By.tagName("option")).size();
+		int randomValue2=rand.nextInt(1,regionSize);
+		regionS.selectByIndex(randomValue2);
+		
+		
+		//ZipCcode
+		int zip=rand.nextInt(10000);
+		String zipStr=""+zip;
 		WebElement zipCode=driver.findElement(By.id("AccountFrm_postcode"));
-		zipCode.sendKeys("21354");
+		zipCode.sendKeys(zipStr);
+		//LoginName
+		int randNum=rand.nextInt(555);
+		String LN=FNames[num1]+LNames[num2]+randNum;
 		WebElement loginName=driver.findElement(By.id("AccountFrm_loginname"));
-		loginName.sendKeys("ShireenBata");
+		loginName.sendKeys(LN);
+		//Password
+		int passInt=rand.nextInt(555);
+		String passStr="GHuyklir78@"+passInt+"!"+"";
 		WebElement password=driver.findElement(By.id("AccountFrm_password"));
-		password.sendKeys("GHuyklir78@");
-		WebElement confirmPassword=driver.findElement(By.id("AccountFrm_confirm"));
-		confirmPassword.sendKeys("GHuyklir78@");
+		password.sendKeys(passStr);
+		//ConfirmPassword
+		WebElement passwordC=driver.findElement(By.id("AccountFrm_confirm"));
+		passwordC.sendKeys(passStr);
+		//CheckBox
 		WebElement check=driver.findElement(By.id("AccountFrm_agree"));
 		check.click();
-		WebElement continue2=driver.findElement(By.xpath("//*[@id=\"AccountFrm\"]/div[5]/div/div/button"));
+		//Conitnue Button
+//		WebElement continue2=driver.findElement(By.xpath("//button[normalize-space()='Continue']"));
+//		continue2.click();
+		//OR BY cssSelectot
+		WebElement continue2=driver.findElement(By.cssSelector(".btn.btn-orange.pull-right.lock-on-click"));
 		continue2.click();
-		
 		
 		
 	}
